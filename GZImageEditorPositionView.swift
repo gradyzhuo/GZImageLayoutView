96a52199961b612c6f8182071aa85308b53989ba
Grady Zhuo <gradyzhuo@gmail.com>
2015-08-27 18:12:01 +0800

    no message

--- GZImageEditorPositionView.swift
+++ GZImageEditorPositionView.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UIKit
 
 internal let GZImageEditorDefaultZoomScale : CGFloat = 1.0
 
--- GZImageLayoutView/GZPositionView.swift
+++ GZImageLayoutView/GZPositionView.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UIKit
 
 //MARK: -
 public class GZPositionView: UIView {
